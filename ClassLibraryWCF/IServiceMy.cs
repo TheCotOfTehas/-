bc8f16d6d60bb6f8639531911536d93671c08282
Евгений <80124915+TheCotOfTehas@users.noreply.github.com>
2023-07-12 08:29:55 +0300

    Добавить интерфейс описывающий работу сервера

---
+++ ClassLibraryWCF/IServiceMy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClassLibraryWCF
+{
+	// ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceMy" в коде и файле конфигурации.
+	[ServiceContract]
+	public interface IServiceMy
+	{
+		[OperationContract]
+		void DoWork();
+	}
+}
---
+++ ClassLibraryWCF/ServiceMy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClassLibraryWCF
+{
+	// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceMy" в коде и файле конфигурации.
+	public class ServiceMy : IServiceMy
+	{
+		public void DoWork()
+		{
+		}
+	}
+}
